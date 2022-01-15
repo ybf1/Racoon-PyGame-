403642de71938d82044934eca2b27002ffda8f88
olesya <olegha2000@gmail.com>
2022-01-15 16:27:44 +0600

    меню и музыка

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import sys
+import os
 
 
 class Cell(pygame.sprite.Sprite):
@@ -56,10 +58,68 @@ class Chip(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 
+class Menu:
+    def __init__(self, items=[400, 350, 'Item', (250, 250, 30), (250, 30, 250)]):
+        self.items = items
+
+    def render(self, surface, font, num_item):
+        for i in self.items:
+            if num_item == i[5]:
+                surface.blit(font.render(i[2], 1, i[4]), (i[0], i[1] - 30))
+            else:
+                surface.blit(font.render(i[2], 1, i[3]), (i[0], i[1] - 30))
+
+    def menu(self):
+        running = True
+        font_menu = pygame.font.Font(None, 50)
+        pygame.key.set_repeat(0, 0)
+        pygame.mouse.set_visible(True)
+        item = 0
+        while running:
+            info.fill((0, 100, 200))
+            screen.fill((0, 100, 200))
+
+            mp = pygame.mouse.get_pos()
+            for i in self.items:
+                if (mp[0] > i[0]) and (mp[0] < i[0] + 155) and (mp[1] > i[1]) and (mp[1] < i[1] + 50):
+                    item = i[5]
+            self.render(screen, font_menu, item)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
+                    if event.key == pygame.K_UP:
+                        if item > 0:
+                            item -= 1
+                    if event.key == pygame.K_DOWN:
+                        if item < len(self.items) - 1:
+                            item += 1
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if item == 0:
+                        running = False
+                        pygame.mixer.music.unpause()
+                    elif item == 1:
+                        exit()
+            screen.blit(info, (0, 0))
+            screen.blit(screen, (0, 30))
+            pygame.display.flip()
+
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((700, 600))
+    info = pygame.Surface((800, 30))
+    items = [(350, 300, 'Play', (11, 0, 77), (250, 250, 30), 0),
+              (350, 340, 'Exit', (11, 0, 77), (250, 250, 30), 1)]
+    game = Menu(items)
+    game.menu()
+    WIDTH, HEIGHT = 700, 600
+    FPS = 60
+    clock = pygame.time.Clock()
+    pygame.mixer.music.load('music.mp3')
+    pygame.mixer.music.play(loops=-1)
 
     all_sprites = pygame.sprite.Group()
     cells = []
@@ -87,6 +147,9 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 dice1.change_image()
                 dice2.change_image()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.mixer.music.pause()
+                game.menu()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     for chip in chips:
@@ -97,15 +160,20 @@ if __name__ == '__main__':
                             else:
                                 chip.cell_number += dice_values[1]
                                 dice_values[1] = 0
-
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+                pygame.mixer.music.pause()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_2:
+                pygame.mixer.music.unpause()
+                pygame.mixer.music.set_volume(0.5)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_3:
+                pygame.mixer.music.unpause()
+                pygame.mixer.music.set_volume(1)
 
         for chip in chips:
             for cell in cells:
                 if cell.number == chip.cell_number:
                     chip.rect.x = cell.rect.x + 5
                     chip.rect.y = cell.rect.y + 5
-
-
 
         screen.fill("black")
         all_sprites.draw(screen)
